828df2ba0bb655a302d557a279fa8c0e02ba7723
DiegoFranciscoHG <DiegoFranciscoHG@users.noreply.github.com>
2019-04-29 18:00:51 +0200

    Added help text.

--- manman.sh
+++ manman.sh
@@ -1,9 +1,15 @@
 answer=
+usage="$(basename $0) [OPTION...] TOPIC... 
+
+  Visualize man pages in a more visual way.
+
+    -h  Show this help text.
+"
 
 while getopts "h" opt; do
   case $opt in
     h)
-      echo "TODO: Add help dialog."
+      echo "$usage"
       exit 1
       ;;
     \?)
@@ -17,7 +23,7 @@ if [ "$1" != "" ]; then
     then
         answer=$1
     else
-        echo -e "\e[91m✗ \e[39mTopic not found. Exiting."
+        echo -e "\e[91m✗ \e[39mTopic not found."
         exit 1
     fi
 else
@@ -27,6 +33,6 @@ fi
 if [ $answer != null ]; then
     { man -Tpdf $answer | zathura --log-level=error - & } 1>/dev/null 2>&1
 else
-    echo -e "\e[91m✗ \e[39mTopic not selected. Exiting."
+    echo -e "\e[91m✗ \e[39mTopic not selected."
     exit 1
 fi
