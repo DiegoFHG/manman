6d9996d4cac07cdf616bd2fd35fe99c68112a7b6
DiegoFranciscoHG <DiegoFranciscoHG@users.noreply.github.com>
2019-04-29 16:10:14 +0200

    Added handling of CLI flags.

--- manman.sh
+++ manman.sh
@@ -1,12 +1,30 @@
 answer=
 
+while getopts "h" opt; do
+  case $opt in
+    h)
+      echo "TODO: Add help dialog."
+      exit 1
+      ;;
+    \?)
+      exit 1
+      ;;
+  esac
+done
+
 if [ "$1" != "" ]; then
-    answer=$1
+    if man "$1" > /dev/null 2>&1
+    then
+        answer=$1
+    else
+        echo -e "\e[91m✗ \e[39mTopic not found. Exiting."
+        exit 1
+    fi
 else
     answer=$(man -k . | rofi -dmenu | awk '{print $1}')
 fi
 
-if [ "$answer" ]; then
+if [ $answer != null ]; then
     { man -Tpdf $answer | zathura --log-level=error - & } 1>/dev/null 2>&1
 else
     echo -e "\e[91m✗ \e[39mTopic not selected. Exiting."
