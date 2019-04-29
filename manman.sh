b5c53152aa4b09424bed2e342dd84f5e7297b01a
DiegoFranciscoHG <DiegoFranciscoHG@users.noreply.github.com>
2019-04-29 15:27:37 +0200

    Added optional argument topic, for opening directly a man page of a specific topic.

--- manman.sh
+++ manman.sh
@@ -1,2 +1,14 @@
-answer=$(man -k . | rofi -dmenu | awk '{print $1}')
-{ man -Tpdf $answer | zathura --log-level=error - & } 1>/dev/null 2>&1
+answer=
+
+if [ "$1" != "" ]; then
+    answer=$1
+else
+    answer=$(man -k . | rofi -dmenu | awk '{print $1}')
+fi
+
+if [ "$answer" ]; then
+    { man -Tpdf $answer | zathura --log-level=error - & } 1>/dev/null 2>&1
+else
+    echo -e "\e[91m✗ \e[39mTopic not selected. Exiting."
+    exit 1
+fi
